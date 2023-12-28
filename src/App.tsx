983771df795e92bc7b292d97a70cde4e909e0d0c
Quach Hieu <hieuquach0813@gmail.com>
2023-12-28 22:21:35 +0700

    code: UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
+import { createContext, useState } from "react";
 import './App.css';
 import '../src/acess/css/layout.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Login from './components/login';
 import {privateRoutes} from './route/index';
+export const AppContext = createContext<any>(null);
 
 function App() {
+  const [spinner, setSpinner] = useState(false);
  //
   return (
     <div className="App">
-      <Router>
-        <Routes>
-        {<Route path="/" element={<Login />} />}
-          {privateRoutes.map((route, index) => {
-            return (
-              <>
-              </>
-            )
-          })}
-        </Routes>
-      </Router>
+      <AppContext.Provider value={{ spinner, setSpinner }}>
+        <Router>
+          <Routes>
+          {<Route path="/" element={<Login />} />}
+            {privateRoutes.map((route, index) => {
+              return (
+                <>
+                </>
+              )
+            })}
+          </Routes>
+        </Router>
+      </AppContext.Provider>
     </div>
   );
 }
--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -21,15 +21,24 @@ const Login = () => {
 
     const onSubmit = async (data: any) => {
         data.userName = data.userName.trim();
+        // const token = await login(data);
+        // if (token) {
+        //     const payload: any = jwt(token || "");
+        //     localStorage.setItem("token", token);
+        //     navigate("/event");
+        // }
     };
 
     return (
-        <>
-            <div className="login">
-                <div className="login-image slide-in-left d-none d-xl-block"></div>
-                <div className="form-login">
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="col-12 mb-2">
+        <div className="login-container bg-white">
+                <div className="bg-login slide-in-left d-none d-xl-block">
+                </div>
+                <div className="login-content position-relative d-flex flex-column align-items-center justify-content-center">
+                    <div className="login-logo scale-in-center position-absolute top-0 w-100 mt-4 mt-xxl-5"></div>
+                    <div className="login-head">Login</div>
+                    <div className="login-form">
+                        <div className="row">
+                            <div className="col-12 mb-2">
                                 <Controller
                                     name="userName"
                                     control={control}
@@ -92,7 +101,42 @@ const Login = () => {
                                     )}
                                 />
                             </div>
-                            <div className="row">
+                        </div>
+                        {/* <div className="row ">
+                            <div className="col-7 d-flex align-items-center ">
+                                <Controller
+                                    name="memorize"
+                                    control={control}
+                                    render={({ field, fieldState }) => (
+                                        <>
+                                            <Checkbox
+                                                inputId={field.name}
+                                                checked={field.value}
+                                                inputRef={field.ref}
+                                                className={"mt-1"}
+                                                style={{ width: "30px" }}
+                                                onChange={(e) =>
+                                                    field.onChange(e.checked)
+                                                }
+                                            />
+                                            <label className="label-field fw-normal">
+                                                {" "}
+                                                Remember Me
+                                            </label>
+                                            <ErrorMessageCustom
+                                                error={errors["memorize"]}
+                                            />
+                                        </>
+                                    )}
+                                />
+                            </div>
+                            <div className="col-5 mt-1 text-end">
+                                <NavLink to={"/forgot-password"}>
+                                    Forgotten password?
+                                </NavLink>
+                            </div>
+                        </div>
+                        <div className="row">
                             <div className="col-12 mt-4">
                                 <Button
                                     type="submit"
@@ -101,10 +145,17 @@ const Login = () => {
                                 />
                             </div>
                         </div>
-                    </form>
+                        <div className="row">
+                            <div className="col-12 text-center mt-2">
+                                <span className="me-1">Don't have an account?</span>
+                                <NavLink to={"/register"}>
+                                    Sign up
+                                </NavLink>
+                            </div>
+                        </div> */}
+                    </div>
                 </div>
             </div>
-        </>
     )
 }
 
